9fd7b65977b37af5d032919ea541c477022f4ee9
kianeblackman <kianeblackman@gmail.com>
2022-12-09 16:22:22 +0000

    debugging in searchList

--- src/components/SearchedList/SearchedList.js
+++ src/components/SearchedList/SearchedList.js
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
-import { searchContext } from "../searchContext";
+import React, { useContext } from 'react';
+import { searchContext } from '../searchContext';
 
 export default function SearchedList() {
-  const { search, setSearch } = useContext(searchContext);
+  const { search } = useContext(searchContext);
   console.log(search);
 
   return (
